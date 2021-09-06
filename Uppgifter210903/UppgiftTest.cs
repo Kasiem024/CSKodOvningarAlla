00ec112109574240bfb26116adafb2283439ccbd
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-06 13:11:54 +0200

    etstetstst

---
+++ Uppgifter210903/UppgiftTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        static void Master(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
