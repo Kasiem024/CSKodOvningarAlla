7fa482bd6040220575ab297ae75f7be20105bd40
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-06 14:41:30 +0200

    Test

--- Uppgifter210906/U210906X1/Program.cs
+++ Uppgifter210906/U210906X1/Program.cs
@@ -13,6 +13,29 @@ namespace U210906X1
             Console.WriteLine("Skriv en mening: ");
             String sentence = Console.ReadLine();
 
+            int wordcount = 0;
+            int index = 0;
+
+            while (index < sentence.Length && char.IsWhiteSpace(sentence[index]))
+                index++;
+
+            while (index < sentence.Length)
+            {
+
+                while (index < sentence.Length && !char.IsWhiteSpace(sentence[index]))
+                    index++;
+
+                wordcount++;
+
+                while (index < sentence.Length && char.IsWhiteSpace(sentence[index]))
+                    index++;
+            }
+            Console.WriteLine("Du har skrivit " + wordcount + " ord.");
+
+            foreach (var word in sentence)
+            {
+                Console.WriteLine($"{word}");
+            }
         }
     }
 }
--- Uppgifter210906/U210906X7/Program.cs
+++ Uppgifter210906/U210906X7/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 //Be användaren mata in en mening.
 //Översätt den enligt Leet Speak (när det finns flera alternativ i tabellen, välj själv så det bara är 1-1)
+//Skapa ett nytt publikt repo i GitHub U210906X1
+//OBS skapa ingen ReadMe
+//Anslut nu ditt GitHub repo till din lokala mapp
+//Pusha upp ditt lokala arbete till ditt GitHub repo
+
 
 namespace U210906X7
 {
