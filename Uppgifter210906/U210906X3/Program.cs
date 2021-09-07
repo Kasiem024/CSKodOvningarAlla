629cc4095c69d8536c6d2bd93694b3b525791bf4
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-07 11:42:36 +0200

    experimetner

--- Uppgifter210906/U210906X3/Program.cs
+++ Uppgifter210906/U210906X3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 //Låt användaren mata in en mening.
 //Skriv ut varje unikt ord på en egen rad, och ange hur ofta ordet förekommer (ex "och (2)")
 //Skriv ut varje unikt ord på en egen rad, sorterat efter hur ofta ordet förekommer
@@ -12,7 +14,20 @@ namespace U210906X3
         {
             Console.WriteLine("Skriv en mening: ");
             string sentence = Console.ReadLine();
+            string[] words = sentence.Split(" ");
 
+
+            if (true)
+            {
+
+            }
+            String Tobematched = words[i];
+            int Count = 0;
+            foreach(Match Match in Regex.Matches(sentence, Tobematched, RegexOptions.IgnoreCase))
+            {
+                Count++;
+            }
+            Console.WriteLine("{0}" + " Found " + "{1}" + " Times", Tobematched, Count);
 
 
         }
