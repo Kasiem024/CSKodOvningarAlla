2441feef11f7253b69f4c8eee95d775b5d1e8e6f
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-07 10:26:06 +0200

    Klar?

--- Uppgifter210906/U210906X2/Program.cs
+++ Uppgifter210906/U210906X2/Program.cs
@@ -10,10 +10,26 @@ namespace U210906X2
         static void Main(string[] args)
         {
             Console.WriteLine("Skriv en mening: ");
-            String sentence = Console.ReadLine();
-            String[] words = sentence.Split(" ");//I princip skapar en ny variabel efter varje " " och sparar den. 
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(" ");//I princip skapar en ny variabel efter varje " " och sparar den. 
 
-            Array.Sort(words);
+            Array.Sort(words, (x, y) => y.Length.CompareTo(y.Length));//Sorterar alla variabler som finns i "word" efter antalet karaktärer den har.
+
+            Console.WriteLine("Orden du skrev in kommer nu skrivas ut sorterade i stigande ordning beroende på längden av varje ord: ");
+
+            foreach (var word in words)//Skapar en ny variabel "word". För varje variabel i listan words skapas en ny word variabel.
+            {
+                Console.WriteLine($"{word}");//Word variabeln skrivs ut.
+            }
+
+            Array.Sort(words, (y, x) => x.Length.CompareTo(y.Length));//Sorterar alla variabler som finns i "word" efter antalet karaktärer den har. Denna gång i omvänd ordning från förra.
+
+            Console.WriteLine("Orden du skrev in kommer nu skrivas ut sorterade i fallande ordning beroende på längden av varje ord: ");
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word}");
+            }
         }
     }
 }
