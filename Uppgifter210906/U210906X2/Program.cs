2482811c0715e4b573a7a3182ab9368b30af6ea9
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-08 11:51:55 +0200

    en förklaring för hur .ComapreTo funkar

--- Uppgifter210906/U210906X2/Program.cs
+++ Uppgifter210906/U210906X2/Program.cs
@@ -30,7 +30,56 @@ namespace U210906X2
             {
                 Console.WriteLine($"{word}");
             }
+
+            /*
+            if (x == null)
+            {
+                if (y == null)
+                {
+                    // If x is null and y is null, they're
+                    // equal.
+                    return 0;
+                }
+                else
+                {
+                    // If x is null and y is not null, y
+                    // is greater.
+                    return -1;
+                }
+            }
+            else
+            {
+                // If x is not null...
+                //
+                if (y == null)
+                // ...and y is null, x is greater.
+                {
+                    return 1;
+                }
+                else
+                {
+                    // ...and y is not null, compare the
+                    // lengths of the two strings.
+                    //
+                    int retval = x.Length.CompareTo(y.Length);
+
+                    if (retval != 0)
+                    {
+                        // If the strings are not of equal length,
+                        // the longer string is greater.
+                        //
+                        return retval;
+                    }
+                    else
+                    {
+                        // If the strings are of equal length,
+                        // sort them with ordinary string comparison.
+                        //
+                        return x.CompareTo(y);
+                    }
+                }
+            }*/
         }
     }
 }
-//Har försökt hitta ett sätt utan Array.Sort men det var antaligen svårare än sättet som jag använder just nu.
+//Har försökt hitta ett sätt utan Array.Sort Men det var antaligen svårare än sättet som jag använder just nu.
