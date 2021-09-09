d486b105c02e5e68753dc5f3455c235f37222e19
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-09 09:45:48 +0200

    kommatecken

--- Uppgifter210906/U210906X7/Program.cs
+++ Uppgifter210906/U210906X7/Program.cs
@@ -14,10 +14,10 @@ namespace U210906X7
     {
         static void Main(string[] args)
         {
-            string alphabet = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
-            string[] alphabetSplit = alphabet.Split(" ");
-            string leetSpeak = "4 8 ( |) 3 |# 6 |-| ! _) |( 1 |\\\\/| |\\\\| 0 |> ? |2 5 + |_| \\\\/ \\\\|/ % `/ 7_";
-            string[] leetSpeakSplit = leetSpeak.Split(" ");
+            string alphabet = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z, ";
+            string[] alphabetSplit = alphabet.Split(",");
+            string leetSpeak = "4,8,(,|),3,|#,6,|-|,!,_),|(,1,|\\\\/|,|\\\\|,0,|>,?,|2,5,+,|_|,\\\\/,\\\\|/,%,`/,7_, ";
+            string[] leetSpeakSplit = leetSpeak.Split(",");
             string[,] array2D = new string[26, 2];
 
             for (int i = 0; i <= alphabetSplit.Length - 1; i++)
@@ -26,7 +26,7 @@ namespace U210906X7
                 array2D[i, 1] = leetSpeakSplit[i];
             }
 
-            Console.WriteLine("Skriv en meninig på engelska som du vill översätta till Leet Speak");
+            Console.WriteLine("Skriv en mening på engelska som du vill översätta till Leet Speak.");
             string sentence = Console.ReadLine();
             char[] chars = sentence.ToCharArray();
 
