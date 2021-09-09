f4931455988600ad5b92bc0fffae4fa0d62545a3
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-09 12:20:52 +0200

    ska pröva igen

--- Uppgifter210906/Uppgifter210906X9/Program.cs
+++ Uppgifter210906/Uppgifter210906X9/Program.cs
@@ -6,14 +6,23 @@ namespace Uppgifter210906X9
     {
         static void Main(string[] args)
         {
-            string num = Console.ReadLine();
-            int[] numArr = new int [num.Length];
-
-            for (int i = 0; i < num.Length - 1; i++)
+            int[] arr = { 78, 55, 45, 98, 13 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
             {
-                num[i] = numArr[i];
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
             }
-
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
         }
     }
 }
