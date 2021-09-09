5844d97729b71074015deab337835acab7722f27
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-09 12:19:47 +0200

    jag har tapapt tråden

--- Uppgifter210906/Uppgifter210906X9/Program.cs
+++ Uppgifter210906/Uppgifter210906X9/Program.cs
@@ -6,47 +6,14 @@ namespace Uppgifter210906X9
     {
         static void Main(string[] args)
         {
-            int num = Convert.ToInt32(Console.ReadLine());
-            int numArr = new string [num]
+            string num = Console.ReadLine();
+            int[] numArr = new int [num.Length];
 
-            foreach (int number in numArr)
+            for (int i = 0; i < num.Length - 1; i++)
             {
-                Console.WriteLine(number);
+                num[i] = numArr[i];
             }
 
-            bool needsSorting = true;
-            //Gör en loop för varje tal som skall sorteras, avbryt om talen är sorterade
-            for (int i = 0; i < numArr.Length - 1 && needsSorting; i++)
-            {
-                //Signalera att vi börjar om en ny vända med sortering
-                needsSorting = false;
-                //Gå igenom alla tal fram till och med de tal som ev. 
-                //redan är sorterade (variabeln i)
-                for (int j = 0; j < numArr.Length - 1 - i; j++)
-                {
-                    //Flytta större tal fram i vektorn
-                    if (numArr[j] > numArr[j + 1])
-                    {
-                        //Signalera att vi kommer att behöva fortsätta sortera
-                        needsSorting = true;
-                        //Byt plats på tal
-                        int tmp = numArr[j + 1];
-                        numArr[j + 1] = numArr[j];
-                        numArr[j] = tmp;
-                    }
-                }
-                //Har vi nu inte behövt sortera några tal så är 
-                //needsSorting == false och loop'en kommer att avbrytas
-                Console.WriteLine("TEST");
-                foreach (int number in numArr)
-                {
-                    Console.WriteLine(number);
-                }
-
-
-
-
-            }
         }
     }
 }
