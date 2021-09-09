b9d8cd398b22dfb77844689e4077486a75e0c2b9
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-09 12:45:57 +0200

    nästan klar med det andra kodexmeplet

--- Uppgifter210906/Uppgifter210906X9/Program.cs
+++ Uppgifter210906/Uppgifter210906X9/Program.cs
@@ -21,6 +21,35 @@ namespace Uppgifter210906X9
                 Console.WriteLine(number);
             }
 
+            bool needsSorting = true;
+            //Gör en loop för varje tal som skall sorteras, avbryt om talen är sorterade
+            for (int i = 0; i < numArr.Length - 1 && needsSorting; i++)
+            {
+                //Signalera att vi börjar om en ny vända med sortering
+                needsSorting = false;
+                //Gå igenom alla tal fram till och med de tal som ev. 
+                //redan är sorterade (variabeln i)
+                for (int j = 0; j < numArr.Length - 1 - i; j++)
+                {
+                    //Flytta större tal fram i vektorn
+                    if (numArr[j] > numArr[j + 1])
+                    {
+                        //Signalera att vi kommer att behöva fortsätta sortera
+                        needsSorting = true;
+                        //Byt plats på tal
+                        int tmp = numArr[j + 1];
+                        numArr[j + 1] = numArr[j];
+                        numArr[j] = tmp;
+                    }
+                }
+                //Har vi nu inte behövt sortera några tal så är 
+                //needsSorting == false och loop'en kommer att avbrytas
+
+                Console.WriteLine("Dina lista sorterad 2:");
+                foreach (int number in numArr)
+                    Console.Write(number + " ");
+            }
+
             int temp;
             for (int j = 0; j <= numArr.Length - 2; j++)
             {
@@ -34,9 +63,11 @@ namespace Uppgifter210906X9
                     }
                 }
             }
+
             Console.WriteLine("Dina lista sorterad:");
             foreach (int number in numArr)
                 Console.Write(number + " ");
+
         }
     }
 }
