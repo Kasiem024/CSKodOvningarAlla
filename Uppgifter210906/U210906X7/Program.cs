e0b03bd5166b5726dec830ada1284c2f02602831
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-09 11:36:56 +0200

    kommentarer

--- Uppgifter210906/U210906X7/Program.cs
+++ Uppgifter210906/U210906X7/Program.cs
@@ -15,11 +15,12 @@ namespace U210906X7
     {
         static void Main(string[] args)
         {
-            string alphabet = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z, ";
-            string[] alphabetSplit = alphabet.Split(",");
+            string alphabet = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z, ";//Skapar en string med alla bokstäver förutom Å Ä Ö då de inte finns i Leet Speak.
+            string[] alphabetSplit = alphabet.Split(",");//Gör ovan string till en array och splitar den.
             string leetSpeak = "4,8,(,|),3,|#,6,|-|,!,_),|(,1,|\\\\/|,|\\\\|,0,|>,?,|2,5,+,|_|,\\\\/,\\\\|/,%,`/,7_, ";
             string[] leetSpeakSplit = leetSpeak.Split(",");
 
+            //Försökte här göra något med 2D array men insåg senare att det inte behövs.
             /* string[,] array2D = new string[26, 2];
 
             for (int i = 0; i <= alphabetSplit.Length - 1; i++)
@@ -28,7 +29,7 @@ namespace U210906X7
                 array2D[i, 1] = leetSpeakSplit[i];
             }*/
 
-            Console.WriteLine("Skriv en mening på engelska med stora bokstäver som du vill översätta till Leet Speak.");
+            Console.WriteLine("Skriv en mening på engelska med stora bokstäver som du vill översätta till Leet Speak.");//På engelska eftersom å ä ö inte finns.
             char[] letters = Console.ReadLine().ToCharArray();//Console.ReadLine förväntar sig en string men ToCharArray gör vad som matas in till char.
             string[] sentence = new string[letters.Length];//Gör en string som är lika lång som antalet bokstäver i den inmatade meningen.
 
@@ -40,14 +41,15 @@ namespace U210906X7
 
             for (int i = 0; i < sentence.Length; i++)
             {
-                sentence[i] = sentence[i].ToUpper();
+                sentence[i] = sentence[i].ToUpper();//Gör så att datorn läser vad som än matades in till stora bokstäver eftersom string alphabet är bara stora bokstäver.
             }
 
-            string sentenceLeet = "";
+            string sentenceLeet = "";//Skapas innan for loopen så att den kan användas efter den.
 
             for (int i = 0; i < sentence.Length; i++)
             {
-                sentenceLeet += leetSpeakSplit[Array.IndexOf(alphabetSplit, sentence[i])];
+                sentenceLeet += leetSpeakSplit[Array.IndexOf(alphabetSplit, sentence[i])];//Letar i alphabetSplit efter bokstäverna som finns i meningen.
+                                                                                          //alphabetSplit och LeetSplit är parallela, därför fungerar det här.
             }
 
             Console.WriteLine(sentenceLeet);
