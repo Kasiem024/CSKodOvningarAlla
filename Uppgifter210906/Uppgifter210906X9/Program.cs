d7a5ed06dae98caf4a303732cd5a2770fa33a71e
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-09 11:59:50 +0200

    init commit

---
+++ Uppgifter210906/Uppgifter210906X9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uppgifter210906X9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
