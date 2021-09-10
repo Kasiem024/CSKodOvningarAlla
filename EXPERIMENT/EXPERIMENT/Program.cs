1e1674de7f2d9d82e0b9da5ba0a76ba4ac08bad1
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-10 10:59:53 +0200

    prövar list

--- EXPERIMENT/EXPERIMENT/Program.cs
+++ EXPERIMENT/EXPERIMENT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EXPERIMENT
 {
@@ -7,6 +8,27 @@ namespace EXPERIMENT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Car myCar;
+            myCar = new Car();
+            myCar.color = "red";
+            myCar.make = "Volvo";
+            myCar.model = "V70";
+
+            List<Car> myCars = new List<Car>();
+            myCars.Add(myCar);
+
+
+
         }
+    }
+
+    public class Car
+    {
+        public string color;
+        public string make;
+        public string model;
+
+
     }
 }
