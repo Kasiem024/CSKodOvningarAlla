13909296001128bd8dfd0f907104ef0dc0644dbf
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-13 11:03:18 +0200

    init commit 2

--- U210913/U210913X1/Program.cs
+++ U210913/U210913X1/Program.cs
@@ -1,4 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+
+/*
+Vi bygger en enkel applikation för att hantera försäljning av begagnade bilar. 
+Funktionerna som behöver stödjas är: 
+1/registrera en bil för försäljning
+2/redigera uppgifterna för en befintlig bil
+3/ta bort en bil som blivit såld
+4/visa alla bilar som är till salu
+Fundera på domänmodellen, dvs vilka klasser som behövs och vilka properties. Skriv gärna ner det i en textfil, så du har lite "kladdpapper".
+Console-applikation med en loop och följande menyalternativ. Använda en switch för detta, se länken nedan.
+X = Exit, N=Add Car, E=Edit existing Car, D=Remove Car, S=Show all cars
+Hur ska du hantera redigera bil? Användaren bör kunna ange vilken bil som ska redigeras. Hur ska du hantera vad som uppdateras?
+Vad händer om användaren ber att få redigera en bil som inte finns?
+Samma gäller för att ta bort en bil.
+*/
 
 namespace U210913X1
 {
