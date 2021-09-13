7e091d147adb2b5ee21a354730857a02d30f29a2
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-13 10:24:00 +0200

    init commit

---
+++ U210913/U210913X1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace U210913X1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
