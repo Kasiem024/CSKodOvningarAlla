e67637545fd15031088da24cae1e28888ea0bf29
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-17 13:28:06 +0200

    klar? ingen aning om jag gjorde rätt men jag har en seperat klass calcualtor där kalkulationen händer och om man trycker på knapparna skriv något ut i textboxen

--- U210916/U210916X3/Form1.cs
+++ U210916/U210916X3/Form1.cs
@@ -12,28 +12,50 @@ namespace U210916X3
 {
     public partial class Form1 : Form
     {
+        Calculator Engine = new Calculator();
+
         public Form1()
         {
             InitializeComponent();
         }
         private void btnPlus_Click(object sender, EventArgs e)
         {
+            double i = double.Parse(textBox1.Text);
+            double j = double.Parse(textBox2.Text);
 
+            double answer = Engine.Plus(i, j);
+
+            listBox1.Items.Add($"{i} + {j} = {answer}");
         }
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
+            double i = double.Parse(textBox1.Text);
+            double j = double.Parse(textBox2.Text);
 
+            double answer = Engine.Minus(i, j);
+
+            listBox1.Items.Add($"{i} - {j} = {answer}");
         }
 
         private void btnMultiply_Click(object sender, EventArgs e)
         {
+            double i = double.Parse(textBox1.Text);
+            double j = double.Parse(textBox2.Text);
 
+            double answer = Engine.Multiply(i, j);
+
+            listBox1.Items.Add($"{i} * {j} = {answer}");
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
+            double i = double.Parse(textBox1.Text);
+            double j = double.Parse(textBox2.Text);
 
+            double answer = Engine.Divide(i, j);
+
+            listBox1.Items.Add($"{i} / {j} = {answer}");
         }
     }
 }
