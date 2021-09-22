ee31ce7d75ab9bf4c0ce0a596c0b868873da968e
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-22 10:24:19 +0200

    init commit

---
+++ U210921/U210921X2/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace U210921X2
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Km { get; set; }
+        public int Price { get; set; }
+        public int Year { get; set; }
+    }
+}
