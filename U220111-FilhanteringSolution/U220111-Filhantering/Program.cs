b5aad0c73f5ebd474b32aaf948b793aedbb77775
Kasiem Saeed <kasiemsaeed@gmail.com>
2022-01-11 13:43:13 +0100

    done with first exercise

--- U220111-FilhanteringSolution/U220111-Filhantering/Program.cs
+++ U220111-FilhanteringSolution/U220111-Filhantering/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+class Uppgift1
+{
+    public static void Main()
+    {
+        Console.WriteLine("Vad heter du?");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Tryck enter för att läsa upp filen");
+        Console.ReadLine();
+
+        string path = ".\\hello.txt";
+
+        if (!File.Exists(path))
+        {
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(name);
+            }
+        }
+
+        // Open the file to read from.
+        using (StreamReader sr = File.OpenText(path))
+        {
+            string s;
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
