e64e382604bb9ae46c424cbbf6bbfc210ab62566
Kasiem Saeed <kasiemsaeed@gmail.com>
2022-01-11 14:25:15 +0100

    done with exercise 2

--- U220111-FilhanteringSolution/U220111-Filhantering/Program.cs
+++ U220111-FilhanteringSolution/U220111-Filhantering/Program.cs
@@ -1,34 +1,60 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 class Uppgift1
 {
     public static void Main()
     {
-        Console.WriteLine("Vad heter du?");
-        string name = Console.ReadLine();
 
-        Console.WriteLine("Tryck enter för att läsa upp filen");
+        List<Person> people = new List<Person>();
+
+        people.Add(new Person() { FirstName = "Kasiem", LastName = "Saeed" });
+        people.Add(new Person() { FirstName = "Oscar", LastName = "Gustafsson" });
+        people.Add(new Person() { FirstName = "Adam", LastName = "Pousette" });
+
+        foreach (var item in people)
+        {
+            Console.WriteLine($"{item.FirstName} {item.LastName}");
+        }
+
+        Console.WriteLine("Tryck enter för att spara filen");
         Console.ReadLine();
 
-        string path = ".\\hello.txt";
+        string path = ".\\data.bin";
 
-        if (!File.Exists(path))
+        FileStream fs = new FileStream("data.bin", FileMode.Create);
+
+        // Construct a BinaryFormatter and use it to serialize the data to the stream.
+        BinaryFormatter formatter = new BinaryFormatter();
+        try
         {
-            // Create a file to write to.
-            using (StreamWriter sw = File.CreateText(path))
-            {
-                sw.WriteLine(name);
-            }
+            formatter.Serialize(fs, people);
         }
-
-        // Open the file to read from.
-        using (StreamReader sr = File.OpenText(path))
+        catch (SerializationException e)
         {
-            string s;
-            while ((s = sr.ReadLine()) != null)
-            {
-                Console.WriteLine(s);
-            }
+            Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+            throw;
+        }
+        finally
+        {
+            fs.Close();
         }
     }
+
+    private static void AddText(FileStream fs, string value)
+    {
+        byte[] info = new UTF8Encoding(true).GetBytes(value);
+        fs.Write(info, 0, info.Length);
+    }
+}
+
+[Serializable]
+class Person
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
 }
